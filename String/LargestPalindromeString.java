15c4d2340e1800d1e531a534c7b223c11ec2ca6d
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2021-12-16 12:15:16 +0530

    Create LargestPalindromeString.java
    Sample Input:
    41415
    3
    Sample Output:
    91919
    Explanation:
    Lexicographically largest palindrome 
    after 3 changes is "91919"

---
+++ String/LargestPalindromeString.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void stringchange(String str, int k){
+        int s=0;
+        int e=str.length()-1;
+        char[] newstr=str.toCharArray();
+        
+        while(s<e){
+            if(str.charAt(s)!=str.charAt(e)){
+                char max;
+                if(str.charAt(s)>str.charAt(e)){
+                    max=str.charAt(s);
+                }
+                else
+                    max=str.charAt(e);
+            
+            newstr[s]=newstr[e]=max;
+            k--;
+            }
+            
+            s++;
+            e--;
+        }
+        if(k<0){
+            System.out.println("NOT POSSIBLE");
+            return ;
+        }
+        s=0;
+        e=str.length()-1;
+        while(s<=e){
+            if(s==e){
+                if(k==1){
+                    newstr[s]='9';
+                }
+            }
+            if(k>=2 && str.charAt(s)==newstr[s] && str.charAt(e)==newstr[e]){
+                newstr[s]=newstr[e]='9';
+                k-=2;
+            }
+            else if(k>=1 && (str.charAt(s)!=newstr[s])||(str.charAt(e)!=newstr[e])){
+                newstr[s]=newstr[e]='9';
+                k--;
+            } 
+            s++;
+            e--;
+        }
+        
+        System.out.println(newstr);
+        
+    }
+        
+    
