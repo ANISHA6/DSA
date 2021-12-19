0e5349665eae9127f7372c6ab1f58f3bc70b0754
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2021-12-19 18:33:04 +0530

    Time Complexity: O(2 ^N )
    Given an array A of size n and an integer K, return all subsets of A which sum to K.
    Sample Input :
    9 
    5 12 3 17 1 18 15 3 17 
    6
    Sample Output :
    3 3
    5 1
    
    Time Complexity: O(2^N), where N is the size of the input array. Since, for every index i, two
    recursive cases originate, hence the time complexity is O(2N).

---
+++ RecursionIMP3/ReturnSubsetsSumToK.java
@@ -0,0 +1,47 @@
+public class solution {
+
+	// Return a 2D array that contains all the subsets which sum to k
+    
+    public static int[][] helper(int[] input, int si, int k){
+     if(si>input.length-1){
+            if(k==0){
+                return new int[1][0];
+            }else
+                return new int[0][0];
+        }
+        
+          int[][] sol1= helper(input,si+1,k-input[si]);
+          int[][] sol2=helper(input,si+1,k);
+        int r=sol1.length+sol2.length;
+        int[][] output=new int[r][]; // Here only defining size of row
+        int p=0;
+        for(int i=0;i<sol1.length;i++){
+            output[p]=new int[sol1[i].length+1]; // Declaring size of col, same as declaring 1D array
+            output[p][0]=input[si]; //Addind element in sol1
+            
+            for(int j=1;j<=sol1[i].length;j++){
+                output[p][j]=sol1[i][j-1];
+            }
+            p++;
+        }
+        for(int i=0;i<sol2.length;i++){
+            output[p]=new int[sol2[i].length];// Also called jagged array in java
+           
+            
+            for(int j=0;j<sol2[i].length;j++){ //Coping sol2 to main output withut including current element
+                output[p][j]=sol2[i][j];
+            }
+            p++;
+        }
+        return output;
+        
+        
+    }
+	public static int[][] subsetsSumK(int input[], int k) {
+		// Write your code here
+        
+        return helper(input,0,k);
+        
+
+	}
+}
