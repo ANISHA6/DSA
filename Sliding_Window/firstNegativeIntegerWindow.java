a34e8a41e8b39bac120fe6d8e9f1ae64e08d093f
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2021-12-29 20:07:09 +0530

    time o(n) || space o(k)
    Example 1:
    
    Input : 
    N = 5
    A[] = {-8, 2, 3, -6, 10}
    K = 2
    Output : 
    -8 0 -6 -6
    Explanation :
    First negative integer for each window of size k
    {-8, 2} = -8
    {2, 3} = 0 (does not contain a negative integer)
    {3, -6} = -6
    {-6, 10} = -6
     
    Example 2:
    Input : 
    N = 8
    A[] = {12, -1, -7, 8, -15, 30, 16, 28}
    K = 3
    Output :
    -1 -1 -7 -15 -15 0

---
+++ Sliding_Window/firstNegativeIntegerWindow.java
@@ -0,0 +1,39 @@
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long arr[], int n, int k)
+    {
+        ArrayList<Long> list = new ArrayList<>();
+        Queue<Long> queue = new ArrayDeque<>();
+        int i=0;
+        int j=0;
+        long nonNeg = 0;
+        while(j<n){
+            if(arr[j] < 0)
+                queue.add(arr[j]);
+            
+            if(j-i+1<k)
+                j++;
+            else if(j-i+1 == k){
+                // list.add(queue.peek());
+                if(queue.size() == 0)
+                    list.add(nonNeg);
+                else
+                    list.add(queue.peek());
+                
+                if(!queue.isEmpty() && queue.peek() == arr[i])
+                    queue.poll();
+         
+                i++;
+                j++;
+            }
+        }
+        
+        long ans[] = new long[list.size()];
+        for(i=0; i<list.size(); i++)
+            ans[i] = list.get(i);
+            
+        return ans;
+        
+    }
+}
+
