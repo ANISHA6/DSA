21c252d72a9d182933f17f27a2874761d295e016
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-01-05 10:35:53 +0530

    ImplementedStackUsingDoublyLL
    1. You have to design a Stack class in which the user can access and delete the middle element in O(1) running time complexity.
    2. The data members of the Stack class must be public.
    Implement the following public functions:
    1. Constructor:
     This function will initialize the data members.
    2. push:
    This function should take one argument of type int and has return type void. This function should insert an element in the stack. Time complexity should be O(1).
    3. pop:
    This function takes no input arguments and has return type int. This should removes the last element which is entered and return that element as an answer. Return 0 if stack is empty.
    Time complexity should be O(1).    
    4. middle:
    This function takes no input arguments and has return type int. This should return the middle element of stack. Return 0, if the stack is empty. Time Complexity: O(1).
    5. removeMiddle:
    This function takes no input arguments and has a return type int. This should remove and return the data of middle element of stack. Return 0, if the stack is empty.Time Complexity: O(1)
    6. print:
    This function takes no input arguments and has void return type. This should print all the elements of the stack. Time Complexity: O(n) 
    Note: If there are even number of elements in the stack, we should consider the first middle element as the middle one. For example, in case of {1,2,3,4}; 2 is considered as the middle element.
    Input Format:
    The first and only line of input contains series of integers, terminated by -1. The integers represent choice. 
    For choice equal to 1, push function is called. When choice is 1, it is followed by another integer that denotes element to be inserted in the stack.
    For choice equal to 2, pop function is called.
    For choice equal to 3, middle function is called.
    For choice equal to 4, removeMiddle function is called.
    For choice equal to 5, print function is called.
    Constraints:
    Time Limit: 1 second
    Output format:
    The output lines are formatted according to the problem description.
    Sample Input 1:
    1 1 1 2 1 3 1 4 1 5 3 -1
    Sample Output 1:
    3

---
+++ BonusProblemsOfStack&Queue/stackWithMiddleOperations.java
@@ -0,0 +1,112 @@
+public class Stack {
+
+	/********************************
+	
+		STRUCTURE OF NODE CLASS
+
+		class Node {
+    		int data;
+    		Node next;
+    		Node prev;
+    		Node(int data) {
+        		this.data = data;
+        		this.next = null;
+        		this.prev = null;
+    		}
+		}
+
+	*******************************/
+    //for TC:O(!), We have to use doubly LL
+    public Node head;
+
+    public Node middle;
+    int size;
+
+	Stack() {
+
+        head=null;
+        middle=null;
+        size=0;
+	}
+
+	public void push(int element) {
+        Node new_node=new Node(element);
+        if(head==null){
+            head=new_node;
+            middle=new_node;
+            size++;
+            return;
+        }
+        new_node.prev=head;
+       head.next=new_node;//Adding new node right side,for FIFO function
+        head=new_node;//Updating new node as a head
+        size++;
+        if(size%2==1){//In case of odd no. of element
+            middle=middle.next;//Move middle to its next
+        }
+        
+        
+
+	}
+
+	public int pop() {
+		// Return 0 if the stack is empty
+        if(head==null){
+		return 0;  } 
+        
+        int del_data=head.data;
+        head=head.prev;
+        size--;
+        if(size%2==0)
+            middle=middle.prev;
+        return del_data;
+	}
+	
+	public int getmiddle() {
+		// Return 0 if the stack is empty
+        if(middle==null){
+		return 0;   }
+        int mid_data=middle.data;
+        return mid_data;
+        
+	}
+		
+	public int removeMiddle() {
+		// Return 0 if the stack is empty
+        if(middle==null){
+        return 0; }
+        
+        int mid_data=middle.data;
+        Node previous=middle.prev;
+        Node foll=middle.next;
+        size--;
+        if(size%2==1){
+            middle=middle.next;
+            
+        }
+        else{
+            middle=middle.prev;
+        }
+        if(previous!=null){
+            previous.next=foll;//connecting middle previous to middle next or deleting middle element
+        }
+        if(foll!=null){
+            foll.prev=previous;
+        }
+        return mid_data;
+	}
+		
+	public void print() {
+        
+        if(head==null){
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.prev;//Printing right to left (back to front)to follow FIFO sequence of stack
+        }
+			
+	} 
+}
+    
