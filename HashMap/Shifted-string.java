3ee83e0bfcbb419cdfd93b1d8088437ab035a778
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-05 12:48:34 +0530

    Update Shifted-string.java

--- HashMap/Shifted-string.java
+++ HashMap/Shifted-string.java
@@ -1,20 +1,23 @@
+import java.util.*;
 public class Solution {
     public static String getkey(String str){
         String key="";
         
         for(int i=1;i<str.length();i++){
-            int diff=str.charAt(i)-str.charAt(i-1);
+            int diff=str.charAt(i)-str.charAt(i-1);//e.g, a-z=-26
             if(diff<0){
-                diff+=26;
+                diff+=26;//-26+26=0
             }
-            key+=diff+"a";
+            key+=diff+"a";//a
         }
         return key;
     }
 
-	public static void grouping(String[] arr, int n) { 
+	public static void grouping(String[] arr, int n) {
 		// TODO Auto-generated method stub
         HashMap<String,ArrayList<String>> map=new HashMap<>();
+        //here key the diff b/w consecutve characters of string
+        //and value is group of string with ame diff
         
         for(int i=0;i<n;i++){
             String s=arr[i];
@@ -25,14 +28,14 @@ public class Solution {
                 map.put(key,list);
                 
             }else{
-                ArrayList<String> l=map.get(key);
+                ArrayList<String> l=map.get(key);//getting reference of arraylit
                 l.add(s);
             }
         }
             
             for(ArrayList<String> list:map.values()){
                 for(String st:list){
-                    System.out.print(st+" ");
+                    System.out.print(st+" ");//printing one group values in on line
                 }
                 System.out.println();
             }
@@ -40,3 +43,5 @@ public class Solution {
         }
 		
 	}
+
+
