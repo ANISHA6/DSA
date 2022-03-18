98660f11b513ac806061b3471b059446d47af716
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-18 11:57:23 +0530

    TC:O(H), SC:O(H), Where H is the height of the i/p BST
    Given a BST and an integer k. Find if the integer k is present in given BST or not. You have to return true, if node with data k is present, return false otherwise.
    Note: Assume that BST contains all unique elements.
    Input Format:
    The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.   
    The following line of input contains an integer, that denotes the value of k.
    Output Format:
    The first and only line of output contains a boolean value. Print true, if node with data k is present, print false otherwise. 
    Constraints:
    Time Limit: 1 second
    Sample Input 1 :
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    2
    Sample Output 1 :
    true
    Sample Input 2 :
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    12
    Sample Output 2 :
    false

---
+++ Binary-Search-Tree/SearchInBST.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+        
+        if(root==null){
+            return false;
+        }
+        if(root.data==k){
+            return true;
+        }
+         if(root.data>k){
+            return searchInBST(root.left,k);
+        }
+        
+         return searchInBST(root.right,k);
+        
+    
+		
+	}
+}
