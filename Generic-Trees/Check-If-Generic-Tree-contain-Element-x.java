6f690f36889f957a38e4171a2b06afc9e8e0b08c
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-21 09:29:17 +0530

    Create Check-If-Generic-Tree-contain-Element-x.java
    Given a generic tree and an integer x, check if x is present in the given tree or not. Return true if x is present, return false otherwise.
    Input format :
    
    Line 1 : Integer x
    
    Line 2 : Elements in level order form separated by space (as per done in class). Order is -
    
    Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element
    
    Output format : true or false
    
    Sample Input 1 :
    40
    10 3 20 30 40 2 40 50 0 0 0 0 
    Sample Output 1 :
    true
    Sample Input 2 :
    4
    10 3 20 30 40 2 40 50 0 0 0 0 
    Sample Output 2:
    false

---
+++ Generic-Trees/Check-If-Generic-Tree-contain-Element-x.java
@@ -0,0 +1,33 @@
+public class Solution {
+
+/*	TreeNode class 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
+
+		// Write your code here
+        
+        if(root==null){
+            return false;
+        }
+        if(root.data==x){
+            return true;
+        }
+        boolean ans=true;
+        for(int i=0;i<root.children.size();i++){
+            ans= checkIfContainsX(root.children.get(i),x);
+        }
+        return ans;
+
+	}
+	
+}
