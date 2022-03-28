dea1889e4d1efa043028a14a9a29e54fb8d7e665
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-28 11:15:04 +0530

    Added 2nd method's sol^n.
    You are given n number of strings and you have to find the longest common prefix in all those strings.
    Input format:
    The first line of the input gives you the number of strings(n) and the following lines contains the input strings.
    Output Format:
    The only output line contains the longest common prefix of all the strings
    Sample Input:
    4
    Codingninjas
    Coding
    Coders
    Codezen
    Sample Output:
    Cod

--- Recursion/LongestCommonPrefix.java
+++ Recursion/LongestCommonPrefix.java
@@ -1,3 +1,21 @@
+//Method 1
+//Below is the explanation of method 1
+
+// (Divide and conquer):
+// This approach is based on recursion. We first divide the whole array of strings into two parts.
+// Then we will do the same for the left part as well for the right part after that. We will continue
+// doing this until we get all the strings of length 1. Now, we will start conquering by return the
+// common prefix of the left and the right parts.
+// Time Complexity: As we are iterating through all the strings, and for each string, we are iterating
+// through each of the characters. So, the time complexity is, O(number of strings* length of the
+// largest string).
+// Space Complexity: The auxiliary space which we are using, to store the longest prefix is
+// O(length of the largest string* log(number of strings)).
+	
+	
+	
+	
+	
 public class Solution {
     private static String com_prefix(String op1, String op2){
         String out="";
@@ -29,4 +47,23 @@ public class Solution {
         String output=divide(input,0,input.length-1);
         System.out.println(output);
 	}
+}
+
+//Method 2
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)
+            return "";
+	    //Assuming, 1st string is common prefix among all the string
+        String pref=strs[0];
+        
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(pref)!=0){//0 ,means pref is present in strs[i], so no need to remove last character from pref
+                pref=pref.substring(0, pref.length()-1);
+            }
+        }
+        return pref;
+        
+    }
 }
