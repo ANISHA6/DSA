ae893dac72d318f5b17cebc2f812ff459faa843b
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-30 11:08:59 +0530

    Update MinCostPath.java

--- DP/MinCostPath.java
+++ DP/MinCostPath.java
@@ -23,6 +23,53 @@ public class Solution {
         return minCost(input,0,0);
   }
 }
+
+        //Using Recursion+Memoization
+
+ public static int mincostMemo(int[][] ip, int[][] dp, int i,int j ,int m, int n){
+        if(i>=m || j>=n){
+             return Integer.MAX_VALUE;
+         }
+        
+        if(i==m-1&& j==n-1){
+            return dp[i][j];
+        }
+        int ans1,ans2,ans3;
+        if(dp[i+1][j]==Integer.MAX_VALUE){
+            ans1=mincostMemo(ip,dp,i+1,j,m,n);
+            dp[i+1][j]=ans1;
+        }else
+            ans1=dp[i+1][j];
+        
+        if(dp[i][j+1]==Integer.MAX_VALUE){
+            ans2=mincostMemo(ip,dp,i,j+1,m,n);
+            dp[i][j+1]=ans2;
+        }else
+            ans2=dp[i][j+1];
+        if(dp[i+1][j+1]==Integer.MAX_VALUE){
+            ans3=mincostMemo(ip,dp,i+1,j+1,m,n);
+            dp[i+1][j+1]=ans3;
+        }else
+            ans3=dp[i+1][j+1];
+        return ip[i][j]+(Math.min(ans1,Math.min(ans2,ans3)));
+    }
+
+	public static int minCostPath(int[][] input) {
+		//Your code goes here
+      
+        //Using Recursion+Memoization
+         int m=input.length;
+        int n=input[0].length;
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[i].length;j++){
+                dp[i][j]=Integer.MAX_VALUE;
+            }
+        }
+        return mincostMemo(input,dp,0,0,m,n);
+	}
+
+
 // Iterative solution
 // TC:O(M*N), SC:O(M+N)
  int m=input.length;
