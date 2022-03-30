5d5cf29de18e074664a42086257b87fdbcba8dea
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-03-30 13:30:35 +0530

    TC=SC:O(N*M)
    Given two strings S and T with lengths M and N. Find and return the length of their shortest 'Super Sequence'.
     The shortest 'Super Sequence' of two strings is defined as the smallest string possible that will have both the given strings as its subsequences.
    Note :
    If the two strings do not have any common characters, then return the sum of the lengths of the two strings. 
    Input Format:
    The first only line of input contains a string, that denotes the value of string S. The following line contains a string, that denotes the value of string T.
    Output Format:
    Length of the smallest super-sequence of given two strings. 
    Constraints :
    0 <= M <= 10 ^ 3
    0 <= N <= 10 ^ 3
    
    Time Limit: 1 sec
    Sample Input 1 :
    ab
    ac
    Sample Output 1 :
    3
    Explanation of Sample Output 1 :
    Their smallest super sequence can be "abc" which has length = 3.
    Sample Input 2 :
    pqqrpt
    qerepct
    Sample Output 2 :
    9

--- DP/SmallestSuperSequence.java
+++ DP/SmallestSuperSequence.java
@@ -2,7 +2,7 @@ public class Solution {
 
 	public static int smallestSuperSequence(String str1, String str2) {
 
-       
+       //Bottom up approach
         int n1=str1.length();
         int n2=str2.length();
         int[][] dp=new int[n1+1][n2+1];
@@ -28,6 +28,54 @@ public class Solution {
             }
         }
         return dp[0][0];
+        
+        
+        
+}
+}
+
+
+
+        // Top down approach
+        //First fill base condition
+        // filling 1st row
+        for(int i=0;i<=n2;i++){
+            dp[0][i]=i;
+        }
+        
+        //filling 1st col
+        for(int i=0;i<=n1;i++){
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n1][n2];
+        
+        
+        //method 3, 1st find LCS, then, n1+n2-x
+        
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                }
+                else if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        int x= dp[n1][n2];
+        return n1+n2-x;
+        
         
         
         
