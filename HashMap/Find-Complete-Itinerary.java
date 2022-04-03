d1973873721ddf832205380d27c7b0ed50bc8289
Anisha Kumari <49230189+ANISHA6@users.noreply.github.com>
2022-04-03 19:05:38 +0530

    Update Find-Complete-Itinerary.java
    You are given a list of input tickets which are not cyclic and there is only one ticket from every given city except the final destination. You have to find the complete itinerary in order using the given list.
    Input Format:
    The first line of input contains the number of tickets given to you. The second line of input has space-separated tickets with a start and destination city.
    Output Format:
    The output contains the complete itinerary in order in different lines.
    Constraints:
    0<=Number of tickets<=70
    Sample Input:
    6
    Jammu Manali Kashmir Jammu Delhi Chandigarh Amritsar Kashmir Manali Jaipur Chandigarh Amritsar
    Sample Output:
    Delhi -> Chandigarh
    Chandigarh -> Amritsar
    Amritsar -> Kashmir
    Kashmir -> Jammu
    Jammu -> Manali
    Manali -> Jaipur
    Explanation:
    6 tickets are given to us:
    1. Jammu to Manali
    2. Kashmir to Jammu
    3. Delhi to Chandigarh 
    4. Amritsar to Kashmir 
    5. Manali to Jaipur 
    6. Chandigarh to Amritsar
    
    So from these tickets the complete itinerary in order will be:
    
    Delhi -> Chandigarh
    Chandigarh -> Amritsar
    Amritsar -> Kashmir
    Kashmir -> Jammu
    Jammu -> Manali
    Manali -> Jaipur

--- HashMap/Find-Complete-Itinerary.java
+++ HashMap/Find-Complete-Itinerary.java
@@ -3,17 +3,17 @@ public class Solution{
 
 	public static void itineary(HashMap<String, String> input) {
 		// TODO Auto-generated method stub
-        HashMap<String, String> reverseMap=new HashMap<>();        
+        HashMap<String, String> reverseMap=new HashMap<>();
+       
         
         //Reversing the hashmap, to find the starting city
-        for(Map.Entry<String, String> entry=input.entrySet){
-         reversMap.put(entry.getValue(), entry.getKey());
+        for(Map.Entry<String, String> entry:input.entrySet()){
+         reverseMap.put(entry.getValue(), entry.getKey());
         }
-    //Initializing starting point with null
         String str=null;
         
-        for(Map.Entry<String, String> entry=input.entrySet){
-            if(!reverseMap.containsKey(entry.getKey()){
+        for(Map.Entry<String, String> entry:input.entrySet()){
+            if(!reverseMap.containsKey(entry.getKey())){
                 str=entry.getKey();
                 break;
             }
@@ -22,11 +22,12 @@ public class Solution{
         if(str==null){
             return;
         }
-    //if, Starting point is found, then print source to destination with all path
-    //str i source and input.get(str) is destination of that source
-        while(str!=null){
-            System.out.println(str+"->"+input.get(str));
-            str=input.get(str);
+        
+        String to=input.get(str);
+        while(to!=null){
+            System.out.println(str+" -> "+ to);
+            str=to;              
+            to=input.get(to);
         }
        
         
